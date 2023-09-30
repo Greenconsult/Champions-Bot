0858eb3116631d3ab4f5b08da66b258ed6a419e6
Julius Odede <130986642+Greenconsult@users.noreply.github.com>
2023-09-30 19:23:42 +0100

    Update index.tsx from Topheader

--- components/Common/TopHeader/index.tsx
+++ components/Common/TopHeader/index.tsx
@@ -53,16 +53,16 @@ const Index = ({ bgColor }: any) => {
           }`}
         >
           <TopHeaderButton href="/" text="HOME" />
-          <TopHeaderButton href="https://selar.co/m/paraclete?page=1" text="BOOKS" target="_blank" />
+          <TopHeaderButton href="https://www.amazon.co.uk/DONT-NEED-MIRACLE-TOMORROW-TODAY/dp/B08C97TDCX/ref=sr_1_1?crid=CK52WRV6AF3E&keywords=mark+burchell&qid=1686130805&s=books&sprefix=mark+burchell2Cstripbooks2C60&sr=1-1" text="BOOKS" target="_blank" />
           <TopHeaderButton href="https://www.biblegateway.com" text="BIBLE" target="_blank" />
-          <TopHeaderButton href="https://www.youtube.com/@amosfenwatv1905" text="AMOS FENWA TV" target="_blank" />
+          <TopHeaderButton href="https://www.championschurch.org.uk/media" text="Church Media" target="_blank" />
         </div>
         <div className={`xl:hidden shadow-2xl absolute top-[85px] right-0 p-[0px] ${showMenu ? 'block' : 'hidden'}`}>
           <div className="drop-shadow-2xl xl:text-[#4c5758] text-black shadow-2xl md:bg-clip-padding rounded w-[180px] flex items-start gap-[5px] flex-col">
             <TopHeaderButton href="/" text="Home" mblBG={true} />
-            <TopHeaderButton href="https://selar.co/m/paraclete?page=1" text="BOOKS" target="_blank" mblBG={true} />
+            <TopHeaderButton href="https://www.amazon.co.uk/DONT-NEED-MIRACLE-TOMORROW-TODAY/dp/B08C97TDCX/ref=sr_1_1?crid=CK52WRV6AF3E&keywords=mark+burchell&qid=1686130805&s=books&sprefix=mark+burchell2Cstripbooks2C60&sr=1-1" text="BOOKS" target="_blank" mblBG={true} />
             <TopHeaderButton href="https://www.biblegateway.com" text="BIBLE" target="_blank" mblBG={true} />
-            <TopHeaderButton href="https://www.youtube.com/@amosfenwatv1905" text="AMOS FENWA TV" target="_blank" />
+            <TopHeaderButton href="https://www.championschurch.org.uk/media" text="Church Media" target="_blank" />
           </div>
         </div>
       </div>
