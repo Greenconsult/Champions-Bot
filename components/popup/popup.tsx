fd3391522de868cf0948e9712b7bc8a2137b4afd
Julius Odede <130986642+Greenconsult@users.noreply.github.com>
2023-12-02 15:20:59 +0000

    Update popup.tsx

--- components/popup/popup.tsx
+++ components/popup/popup.tsx
@@ -1,1 +1,17 @@
+// PopUp component
+import React from 'react';
 
+interface PopUpProps {
+  onClose: () => void;
+}
+
+const PopUp: React.FC<PopUpProps> = ({ onClose }) => {
+  return (
+    <div className="popup">
+      <p>Would you like to chat?</p>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
+
+export default PopUp;
