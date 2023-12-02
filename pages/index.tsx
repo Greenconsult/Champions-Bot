144282364e356dc5290720a2d22403476802d8bf
Julius Odede <130986642+Greenconsult@users.noreply.github.com>
2023-12-02 15:37:08 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import PopUp from '../components/popup/PopUp'; // Adjust the path based on your folder structure
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
@@ -11,16 +12,6 @@ import TopHeader from '../components/Common/TopHeader/index';
 import Footer from '../components/Footer';
 import langchainAppStore from '../stores';
 import Image from 'next/image';
-
-// PopUp component
-const PopUp = ({ onClose }) => {
-  return (
-    <div className="popup">
-      <p>Would you like to chat?</p>
-      <button onClick={onClose}>Close</button>
-    </div>
-  );
-};
 
 const Home: NextPage = () => {
   const setSelectedList = langchainAppStore((state: any) => state.informationStore.setSelectedList);
@@ -55,56 +46,6 @@ const Home: NextPage = () => {
             <div>
               <Image src={bannerImg} className="w-full" alt="Banner" />
             </div>
-            {/* <Slider
-              {...SlickSetting}
-              afterChange={(currentTarget) => {
-                if (currentTarget === 0) {
-                  setSelectedList({
-                    namespace: 'hcc',
-                    title: 'Holyghost Christian Centre',
-                    type: '0'
-                  });
-                } else {
-                  setSelectedList({
-                    namespace: 'hcc',
-                    title: 'Amos Fenwa Ministries',
-                    type: '1'
-                  });
-                }
-                console.log(currentTarget);
-              }}
-            >
-              <div className="">
-                <SlickComponent
-                  header="HCC Worldwide"
-                  paragraph="Hi, welcome to the chatbot app for the Holyghost Christian Centre (HCC). HCC is a global church with branches around the world."
-                  img="../../../assets/img/icon/7.JPG"
-                />
-              </div>
-              <div className="">
-                <SlickComponent
-                  header="Amos Fenwa Ministries"
-                  paragraph="Hi, welcome to the chatbot app for Amos Fenwa Ministries. Amos Fenwa Ministries is a distinguished ministry 
-                    with a clear vision to raise and impact destinies."
-                  img="../../../assets/img/icon/2.jpg"
-                />
-              </div>
-              <div className="">
-                <SlickComponent header="" paragraph="" img="../../../assets/img/icon/3.jpg" />
-              </div>
-              <div className="">
-                <SlickComponent header="" paragraph="" img="../../../assets/img/icon/1.jpeg" />
-              </div>
-              <div className="">
-                <SlickComponent header="" paragraph="" img="../../../assets/img/icon/5.jpg" />
-              </div>
-              <div className="">
-                <SlickComponent header="" paragraph="" img="../../../assets/img/icon/6.jpg" />
-              </div>
-              <div className="">
-                <SlickComponent header="" paragraph="" img="../../../assets/img/icon/4.jpg" />
-              </div>
-            </Slider> */}
             <div className="flex-col justify-start items-start mt-10 gap-6 xl:gap-10 absolute left-[10%] sm:bottom-[30px] lg:bottom-0 z-[9999]">
               <div className="max-w-[225px] w-full">
                 <Link href="/qa">
@@ -120,6 +61,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
+      {showPopUp && <PopUp onClose={closePopUp} />}
     </>
   );
 };
